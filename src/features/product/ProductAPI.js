5b1adf9e152e32c785d1cd04c0409e4c8218123d
RaviSharma <rvssharma11@gmail.com>
2023-07-13 09:08:32 +0530

    part-5 redux setup ,adding fiters and sorting

--- src/features/product/ProductAPI.js
+++ src/features/product/ProductAPI.js
@@ -1,9 +1,26 @@
-// A mock function to mimic making an async request for data
-export function  fetchCount(amount = 1) {
+export function fetchAllProducts() {
   return new Promise(async (resolve) =>{
-    const res=await fetch('http://localhost:8080')
-    const data=await res.json()
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/products') 
+    const data = await response.json()
     resolve({data})
   }
-  ); 
+  );
+}
+
+export function fetchProductsByFilters(filter) {
+  // filter = {"category":"smartphone"}
+  // TODO : on server we will support multi values
+  let queryString = '';
+  for(let key in filter){
+    queryString += `${key}=${filter[key]}&`
+  }
+
+  return new Promise(async (resolve) =>{
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/products?'+queryString) 
+    const data = await response.json()
+    resolve({data})
+  }
+  );
 }
--- src/features/product/components/ProductDetail.js
+++ src/features/product/components/ProductDetail.js
@@ -1,3 +1,4 @@
+
 import { useState } from 'react'
 import { StarIcon } from '@heroicons/react/20/solid'
 import { RadioGroup } from '@headlessui/react'
@@ -267,7 +268,7 @@ export default function ProductDetail() {
                 type="submit"
                 className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
-                Add to bag
+                Add to Cart
               </button>
             </form>
           </div>
