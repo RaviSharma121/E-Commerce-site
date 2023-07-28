c3f02749d6dbdebad32d7794a712c28cc6273e07
RaviSharma <20uec104@lnmiit.ac.in>
2023-07-28 21:41:28 +0530

    part-18 vercel deployment

--- src/features/order/orderAPI.js
+++ src/features/order/orderAPI.js
@@ -1,45 +1,44 @@
 export function createOrder(order) {
-    return new Promise(async (resolve) => {
-      const response = await fetch('http://localhost:8080/orders', {
-        method: 'POST',
-        body: JSON.stringify(order),
-        headers: { 'content-type': 'application/json' },
-      });
-      const data = await response.json();
-      resolve({ data });
+  return new Promise(async (resolve) => {
+    const response = await fetch('/orders', {
+      method: 'POST',
+      body: JSON.stringify(order),
+      headers: { 'content-type': 'application/json' },
     });
-  }
-  
-  export function updateOrder(order) {
-    return new Promise(async (resolve) => {
-      const response = await fetch('http://localhost:8080/orders/'+order.id, {
-        method: 'PATCH',
-        body: JSON.stringify(order),
-        headers: { 'content-type': 'application/json' },
-      });
-      const data = await response.json();
-      resolve({ data });
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function updateOrder(order) {
+  return new Promise(async (resolve) => {
+    const response = await fetch('/orders/'+order.id, {
+      method: 'PATCH',
+      body: JSON.stringify(order),
+      headers: { 'content-type': 'application/json' },
     });
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function fetchAllOrders(sort, pagination) {
+ let queryString = '';
+
+ for (let key in sort) {
+  queryString += `${key}=${sort[key]}&`;
+}
+  for (let key in pagination) {
+    queryString += `${key}=${pagination[key]}&`;
   }
-  
-  export function fetchAllOrders(sort, pagination) {
-   let queryString = '';
-  
-   for (let key in sort) {
-    queryString += `${key}=${sort[key]}&`;
-  }
-    for (let key in pagination) {
-      queryString += `${key}=${pagination[key]}&`;
-    }
-  
-    return new Promise(async (resolve) => {
-      //TODO: we will not hard-code server URL here
-      const response = await fetch(
-        'http://localhost:8080/orders?' + queryString
-      );
-      const data = await response.json();
-      const totalOrders = await response.headers.get('X-Total-Count');
-      resolve({ data: { orders: data, totalOrders: +totalOrders } });
-    });
-  }
-  
+
+  return new Promise(async (resolve) => {
+    //TODO: we will not hard-code server URL here
+    const response = await fetch(
+      '/orders?' + queryString
+    );
+    const data = await response.json();
+    const totalOrders = await response.headers.get('X-Total-Count');
+    resolve({ data: { orders: data, totalOrders: +totalOrders } });
+  });
+}
